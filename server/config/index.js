bdeac94fe0e3ead2725a9113727c2d2f1e72d5f4
Haneem <kmhaneem00@gmail.com>
2023-07-09 10:30:38 +0530

    added config file

---
+++ server/config/index.js
@@ -0,0 +1,11 @@
+export const MONGO_URI =
+  "mongodb+srv://insafinhaam732:gLpeRK1S9rNwJDBG@cluster0.q2c4rhk.mongodb.net/";
+export const PORT = process.env.PORT || 8000;
+export const APP_SECRET = "Railway App";
+// export const CLOUDINARY_CLOUD_NAME = "devxmy";
+// export const CLOUDINARY_API_KEY = "166351853751398";
+// export const CLOUDINARY_API_SECRET = "jZHgxAngJJYIoRp7ACz_GyXv8S0";
+
+export const CLOUDINARY_API_KEY = "985779228128127";
+export const CLOUDINARY_API_SECRET = "hneTgpHcoVfyFt-ye76F8IIsOEA";
+export const CLOUDINARY_CLOUD_NAME = "dp6yyczpu";
